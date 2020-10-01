f232132b2f917fa4907fc745fcd69e119bc30c42
Wesley_Johnson <johnsonwesley167@gmail.com>
2020-10-01 10:49:26 +0530

    006 - Move player horizontally and Flip charater sprite.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    //Config paras
+    [SerializeField] float runSpeed = 10f;
+
+    //States
+
+    //Cached reference
+    Rigidbody2D myRigidbody;
+    
+    
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Math.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+}
